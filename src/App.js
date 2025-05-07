f358c85885551af07719c860de90afa1ee156973
rajitha.chowdary <rajitha.chowdary@cognitivzen.com>
2025-05-07 11:24:30 +0530

    teams sdk added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,27 @@ import Home from "./Components/Home";
 import Create from "./Components/Create";
 import Read from "./Components/Read";
 import Update from "./Components/Update";
+import * as microsoftTeams from "@microsoft/teams-js";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [context, setContext] = useState(null);
+
+  useEffect(() => {
+    microsoftTeams.app
+      .initialize()
+      .then(() => {
+        console.log("Teams SDK initialized");
+        microsoftTeams.app.getContext().then((context) => {
+          setContext(context);
+          console.log("Teams Context:", context);
+        });
+      })
+      .catch((error) => {
+        console.error("Error initializing Teams SDK:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
